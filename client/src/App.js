054cf4d707fbe8cd1e0d644c7a2a8c6f9561d31b
sigma21 <semihkocakel@hotmail.com>
2019-08-08 11:11:36 +0200

    profile display

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,9 @@ import CreateProfile from "./components/profile-forms/CreateProfile";
 import EditProfile from "./components/profile-forms/EditProfile";
 import AddExperience from "./components/profile-forms/AddExperience";
 import AddEducation from "./components/profile-forms/AddEducation";
+import Profiles from "./components/profiles/Profiles";
+import Profile from "./components/profile/Profile";
+
 if (localStorage.token) {
   setAuthToken(localStorage.token);
 }
@@ -37,6 +40,8 @@ const App = () => {
             <Switch>
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <Route exact path="/profiles" component={Profiles} />
+              <Route exact path="/profile/:id" component={Profile} />
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
               <PrivateRoute exact path="/create-profile" component={CreateProfile} />
               <PrivateRoute exact path="/edit-profile" component={EditProfile} />
